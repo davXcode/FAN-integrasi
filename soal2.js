a31d6aa68a374c4d6dbbe118a52b62648f3279e4
davXcode <surys36@gmail.com>
2022-05-31 19:51:33 +0700

    fix regex

--- soal2.js
+++ soal2.js
@@ -1,11 +1,12 @@
-const specialTest = (input) => {
+const testDua = (input) => {
   const arrInput = input.split(" ");
-  const regex = /[$&+,:;=@#|'<>.-^*()%!]/;
+  const regex = /[*_/(/[!/=/&]/;
   let count = 0;
   for (let i = 0; i < arrInput.length; i++) {
     if (arrInput[i].match(regex)) {
-      count++;
+      continue;
     }
+    count++;
   }
 
   return count;
@@ -15,6 +16,6 @@ const a = "Saat meng*ecat tembok, Agung dib_antu oleh Raihan.";
 const b = "Berapa u(mur minimal[ untuk !mengurus ktp?";
 const c = "Masing-masing anak mendap(atkan uang jajan ya=ng be&rbeda.";
 
-console.log(specialTest(a));
-console.log(specialTest(b));
-console.log(specialTest(c));
+console.log(testDua(a));
+console.log(testDua(b));
+console.log(testDua(c));
